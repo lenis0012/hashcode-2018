9bffc367f4bf9ac09ab1ecd89b4801fc78043940
Lennart ten Wolde <ltwolde@quintor.nl>
2018-03-02 03:59:30 -0100

    Print betetr vehicle

--- src/main/java/nl/tryagain/cars/Vehicle.java
+++ src/main/java/nl/tryagain/cars/Vehicle.java
@@ -1,10 +1,12 @@
 package nl.tryagain.cars;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@ToString
 public class Vehicle {
     @Getter
     private List<Ride> rides = new ArrayList<>();
